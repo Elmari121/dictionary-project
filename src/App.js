a2b9a73bd996655e4a47ead52158ce56d475b85b
Elmari121 <robbertselmari@gmail.com>
2025-06-01 11:05:00 +0200

    SEARCH

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from "./dictionary-logo.png";
 import './App.css';
-
+import Dictionary from "./dictionary.js";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-       <h1>Hello world</h1>
+      
          
       </header>
+      <main>
+        <Dictionary/>
+      </main>
+     <footer className="App-footer">Coded by Elmari Robberts</footer>
+      </div>
     </div>
   );
 }
---
+++ src/dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "./dictionary.css"
+
+export default function Dictionary() { 
+    const [keyword, setKeyword] = useState(null);
+
+    function search(event) {
+        event.preventDefault();
+        alert("searching...");
+    }
+
+    function handleKeyWordChange(event) {
+        setKeyword(event.target.value);
+    }
+
+    return (
+        <div className="dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeyWordChange} />
+            </form>
+        </div>
+    );
+}
+
